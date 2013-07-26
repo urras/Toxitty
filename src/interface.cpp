f484e9c0ed155243d215ed36dcb5e62c32b2e8cd
Kamil Chojnowski <diath@pyboard.net>
2013-07-26 16:12:24 -0400

    Fix segfault in empty buffers.

--- src/interface.cpp
+++ src/interface.cpp
@@ -33,22 +33,24 @@ void Interface::draw()
 	(void) caret;
 
 	unsigned int size = buffers->getSize(buffer);
-	if(size < (unsigned int) height)
+	if(size > 0)
 	{
-		for(int i = 0; i < height; ++i)
+		if(size < (unsigned int) height)
 		{
-			mvprintw(i, 0, "%s\n", buffers->getData(buffer, i).c_str());
+			for(int i = 0; i < height; ++i)
+			{
+				mvprintw(i, 0, "%s\n", buffers->getData(buffer, i).c_str());
+			}
 		}
-	}
-	else
-	{
-		int start = size - height + 1;
-		int p = 0;
-		for(int i = start; i < (int) size; ++i)
+		else
 		{
-			mvprintw(p, 0, buffers->getData(buffer, i).c_str());
-			//mvprintw(p, 0, "p: %d, i: %d\n", p, i);
-			++p;
+			int start = size - height + 1;
+			int p = 0;
+			for(int i = start; i < (int) size; ++i)
+			{
+				mvprintw(p, 0, buffers->getData(buffer, i).c_str());
+				++p;
+			}
 		}
 	}
 
