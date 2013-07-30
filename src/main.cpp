2d37bd6ec407cb864cdd34fa1815b66841caf4cd
Kamil Chojnowski <diath@pyboard.net>
2013-07-30 19:18:21 -0400

    Add input history limit.

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,9 @@ int main(int argc, char *argv[])
 				}
 
 				input->history[currentBuffer].push_back(input->data[currentBuffer]);
+				if(input->history[currentBuffer].size() >= 500)
+					input->history[currentBuffer].erase(input->history[currentBuffer].begin());
+
 				input->setPosHistory(currentBuffer, -1);
 				input->setPosCaret(currentBuffer, -1);
 
