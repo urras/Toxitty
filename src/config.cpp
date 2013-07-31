c9be4aac4c88f60d20de11293ff5bcc7e0e06aa3
Kamil Chojnowski <diath@pyboard.net>
2013-07-30 21:23:23 -0400

    Add config manager.

---
+++ src/config.cpp
@@ -0,0 +1,106 @@
+/*
+	Toxitty - An experimental Tox client.
+	Copyright (C) 2013  Kamil Chojnowski <diath@pyboard.net>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "config.hpp"
+
+Config::Config()
+{
+}
+
+Config::~Config()
+{
+}
+
+bool Config::load()
+{
+	std::ifstream data("config.json");
+	Json::Value root;
+	Json::Reader reader;
+
+	if(!reader.parse(data, root))
+	{
+		buffers->append(Buffers::CoreBuffer, "[!] Failed to load the config file, using defaults.");
+		return false;
+	}
+
+	for(unsigned int i = 0; i < root.size(); ++i)
+	{
+		const Json::Value node = root[i];
+		if(node.isNull() || node.size() != 2)
+			continue;
+
+		m_values[node.get("key", "").asString()] = node.get("value", "").asString();
+	}
+
+	data.close();
+	return true;
+}
+
+bool Config::save()
+{
+	std::ofstream data("config.json");
+	Json::Value root;
+	Json::StyledWriter writer;
+
+	if(!data.is_open())
+	{
+		buffers->append(Buffers::CoreBuffer, "[!] Failed to save the config file.");
+		return false;
+	}
+
+	int counter = 0;
+	for(std::map<std::string, std::string>::iterator it = m_values.begin(); it != m_values.end(); ++it, ++counter)
+	{
+		Json::Value node;
+		node["key"] = it->first;
+		node["value"] = it->second;
+
+		root[counter] = node;
+	}
+
+	data << writer.write(root);
+	return true;
+}
+
+void Config::setValue(const std::string &key, const std::string &value)
+{
+	m_values[key] = value;
+}
+
+std::string Config::getValue(const std::string &key)
+{
+	std::map<std::string, std::string>::iterator it = m_values.find(key);
+	if(it != m_values.end())
+		return it->second;
+
+	return "";
+}
+
+int Config::getIntValue(const std::string &key)
+{
+	std::string value = getValue(key);
+	if(value.empty())
+		return -1;
+
+	return atoi(value.c_str());
+}
+
+bool Config::getBoolValue(const std::string &key)
+{
+	return getIntValue(key) != 0;
+}
---
+++ src/config.hpp
@@ -0,0 +1,50 @@
+/*
+	Toxitty - An experimental Tox client.
+	Copyright (C) 2013  Kamil Chojnowski <diath@pyboard.net>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <cstdlib>
+#include <fstream>
+#include <map>
+#include <string>
+
+#include <jsoncpp/json/reader.h>
+#include <jsoncpp/json/writer.h>
+
+#include "buffers.hpp"
+
+class Config
+{
+	public:
+		Config();
+		~Config();
+
+		bool load();
+		bool save();
+
+		void setValue(const std::string &key, const std::string &value);
+		std::string getValue(const std::string &key);
+		int getIntValue(const std::string &key);
+		bool getBoolValue(const std::string &key);
+
+	private:
+		std::map<std::string, std::string> m_values;
+};
+
+#endif
