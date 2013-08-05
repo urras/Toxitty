d72d0fbe1fcde1c046ae37c0b176ab3fdf1866ab
Kamil Chojnowski <diath@pyboard.net>
2013-08-05 16:12:25 -0400

    Initialize ncurses color pairs if terminal supports colors.

--- src/interface.cpp
+++ src/interface.cpp
@@ -46,6 +46,19 @@ void Interface::init()
 	nodelay(stdscr, 1);
 	curs_set(0);
 
+	if(has_colors())
+	{
+		start_color();
+
+		init_pair(1, COLOR_RED,     COLOR_BLACK);
+		init_pair(2, COLOR_GREEN,   COLOR_BLACK);
+		init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
+		init_pair(4, COLOR_BLUE,    COLOR_BLACK);
+		init_pair(5, COLOR_CYAN,    COLOR_BLACK);
+		init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+		init_pair(7, COLOR_WHITE,   COLOR_BLACK);
+	}
+
 	onResize();
 
 	running = true;
